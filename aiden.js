0409bb8e5a3a0c83fa4ad61c8581652c62d09e39
SEWOONG LEE <dltpdnd1234@gmail.com>
2023-02-25 22:15:10 -0600

    add Description

--- aiden.js
+++ aiden.js
@@ -1,22 +1,26 @@
+// Defines a function that opens a specific GitHub repository
 function linkCloud(){
   window.open("https://github.com/luthentic/Cloudy")
 }
 
+// Defines a function that opens a specific GitHub repository
 function linkTrans(){
   window.open("https://github.com/luthentic/TranscationSystemDesgin")
 }
 
+// Defines a function that opens a specific GitHub repository
 function linkPoker(){
   window.open("https://github.com/luthentic/PokerGame")
 }
 
+// Defines a function that opens a specific GitHub repository
 function linkMall(){
   window.open("https://github.com/luthentic/AidenMall")
 }
 
+// Selects two cursor elements and updates their position to follow the mouse
 let cursor = document.querySelector(".cursor");
-    let cursor2 = document.querySelector(".cursor2");
-
-      document.addEventListener("mousemove",(e)=>{
-      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px;top:" + e.clientY +"px";
-    });
+let cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove",(e)=>{
+  cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px;top:" + e.clientY +"px";
+});
