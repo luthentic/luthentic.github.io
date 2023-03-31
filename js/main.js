7d7c410b0206a33fccb9b33ae8d0bd49251bcf8a
SEWOONG LEE <dltpdnd1234@gmail.com>
2023-03-31 13:11:04 -0600

    Add Cursor Effect

--- js/main.js
+++ js/main.js
@@ -128,6 +128,13 @@
 
 	};
 
+  let cursor = document.querySelector(".cursor");
+    let cursor2 = document.querySelector(".cursor2");
+
+      document.addEventListener("mousemove",(e)=>{
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px;top:" + e.clientY +"px";
+    });
+
 
 	// Loading page
 	var loaderPage = function() {
